b00c7bdcec244a4ca8dcd3b8bcd0d12896a7273a
FrostSnow0219 <andreslopez0219.github@gmail.com>
2023-09-27 18:32:04 -0700

    init

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+  },
+
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "^0.8.15",  
+      settings: {         
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  },
+
+};
